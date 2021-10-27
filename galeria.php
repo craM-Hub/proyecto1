66fe33d671bb265a4607f35a71e56af2abe49a83
craM-Hub <anarka84@gmail.com>
2021-10-27 09:40:14 +0200

    v6 sin terminar, no se sube el archivo

--- galeria.php
+++ galeria.php
@@ -1,6 +1,10 @@
 <?php
 $title = "Galería";
 require_once "./utils/utils.php";
+require_once "./utils/File.php";
+require_once "./utils/SimpleImage.php";
+require_once "./entity/ImagenGaleria.php";
+require_once "./exceptions/FileException.php";
 
 //Variables
 
@@ -9,7 +13,51 @@ $descriptionError = $imagenErr = $hayErrores = false;
 $errores = [];
 
 if ("POST" === $_SERVER["REQUEST_METHOD"]) {
-    //comprobamos que llegan los datos
+    //Procesamos el campo de tipo file
+    try {
+        //Nunca confiar en que lleguen todos los datos
+        if (empty($_POST)) {
+            throw new FileException("Se ha producido un error al procesar el formulario");
+        }
+        $imageFile = new File("imagen", array("image/jpeg", "image/jpg", "image/png"), (2 * 1024 * 1024));
+        $imageFile->saveUploadedFile(ImagenGaleria::RUTA_IMAGENES_GALLERY);
+        try {
+
+            // Create a new SimpleImage object
+            $simpleImage = new \claviska\SimpleImage();
+            $simpleImage
+                ->fromFile(ImagenGaleria::RUTA_IMAGENES_GALLERY . $imageFile->getFileName())
+                ->resize(975, 525)
+                ->toFile(ImagenGaleria::RUTA_IMAGENES_PORTFOLIO . $imageFile->getFileName())
+                ->resize(650, 350)
+                ->toFile(ImagenGaleria::RUTA_IMAGENES_GALLERY . $imageFile->getFileName());
+        } catch (Exception $err) {
+            $errores[] = $err->getMessage();
+            $imagenErr = true;
+        }
+    } catch (FileException $fe) {
+        $errores[] = $fe->getMessage();
+        $imagenErr = true;
+    }
+    $description = sanitizeInput(($_POST["description"] ?? ""));
+    if (empty($description)) {
+        $errores[] = "La descripción es obligatoria";
+        $descriptionError = true;
+    }
+
+    if (0 == count($errores)) {
+        $info = "Imagen enviada correctamente: ";
+        $urlImagen = ImagenGaleria::RUTA_IMAGENES_GALLERY . $imageFile->getFileName();
+        //reseteamos datos del formulario
+        $description = "";
+    } else {
+        $info = "Datos erróneos";
+    }
+
+
+
+
+    /* //comprobamos que llegan los datos
     if (empty($_POST)) {
         $errores[] = 'Se ha producido un error al procesar el formulario';
         $imagenErr = true;
@@ -25,7 +73,7 @@ if ("POST" === $_SERVER["REQUEST_METHOD"]) {
         }
     }
 
-    if (isset($_FILES['imagen']) && ($_FILES['imagen']['error'] === UPLOAD_ERR_OK)) {
+    if (isset($_FILES['imagen']) && ($_FILES['imagen']['error'] == UPLOAD_ERR_OK)) {
         if ($_FILES['imagen']['size'] > (2 * 1024 * 1024)) {
             $errores[] = 'El archivo no puede superar los 2MB';
             $imagenErr = true;
@@ -42,15 +90,15 @@ if ("POST" === $_SERVER["REQUEST_METHOD"]) {
         if (!$imagenErr) {
             //mover si no hay errores
             if (false === move_uploaded_file(
-                $_FILES['imagen']['tmp_name'],
-                "images/index/gallery/" . $_FILES['imagen']['name']
+                $_FILES["imagen"]["tmp_name"],
+                "images/index/gallery/" . $_FILES["imagen"]["name"]
             )) {
                 $errores[] = "Se ha producido un error al mover la imagen";
                 $imagenErr = true;
             }
         }
     } else {
-        $errores[] = "Se ha producido un error. Código de error: " . $_FILES['imagen']['error'];
+        $errores[] = "Se ha producido un error. Código de error: " . $_FILES["imagen"]["error"];
         $imagenErr = true;
     }
 
@@ -61,13 +109,13 @@ if ("POST" === $_SERVER["REQUEST_METHOD"]) {
     //si no hay errores
     if (!$hayErrores) {
         $info = "Imagen enviada correctamente.";
-        $urlImagen = "images/index/gallery/" . $_FILES['imagen']['name'];
+        $urlImagen = "images/index/gallery/" . $_FILES["imagen"]["name"];
 
         //reseteamos datos del formulario
         $description = "";
     } else {
         $info = "Datos erróneos";
-    }
+    } */
 }
 
 include("./views/galeria.view.php");
---
+++ utils/File.php
@@ -0,0 +1,72 @@
+<?php
+require_once __DIR__ . "/../exceptions/FileException.php";
+require_once __DIR__ . "/utils.php";
+
+class File
+{
+    private $file;
+
+    private $fileName;
+
+    public function __construct(
+        string $fileInput,
+        array $mimeTypes = [],
+        int $maxSize = 0
+    ) {
+        $this->file = ($_FILES[$fileInput] ?? "");
+
+        /*
+            Puede ser que si ha habido algún problema en la subida, la variable
+            $_FILES[$fileName] y, por tanto, $this->file, no esté informada
+        */
+
+        if (empty($this->file)) {
+            throw new FileException("Se ha producido un error al procesar el formulario.");
+        }
+        if ($this->file["error"] !== UPLOAD_ERR_OK) {
+            switch ($this->file["error"]) {
+                case UPLOAD_ERR_NO_FILE:
+                    throw new FileException("Debes seleccionar un fichero");
+                    break;
+                case UPLOAD_ERR_INI_SIZE:
+                case UPLOAD_ERR_FORM_SIZE:
+                    throw new FileException("No se ha podido subir el fichero completo");
+                    break;
+                default:
+                    throw new FileException("No se ha podido subir el fichero");
+            }
+        }
+        if (false === in_array($this->file["type"], $mimeTypes)) {
+            throw new FileException("EL tipo de fichero no está soportado");
+        }
+        if (($maxSize > 0) && ($this->file["size"] > $maxSize)) {
+            throw new FileException("El fichero no puede superar $maxSize bytes");
+        }
+        $this->fileName = sanitizeInput($this->file["name"]);
+    }
+
+    public function getFileName(): string
+    {
+        return $this->fileName;
+    }
+
+    public function saveUploadedFile(string $destPath)
+    {
+
+        //Comprobar que el archivo se ha subido mediante formulario
+        if (false === is_uploaded_file($this->file["tmp_name"])) {
+            throw new FileException("El archivo no se ha subido mediante un formulario");
+        }
+
+        $ruta = $destPath . $this->getFileName();
+
+        if (true === is_file($ruta)) {
+            $idUnico = time();
+            $this->fileName = $idUnico . "_" . $this->getFileName();
+            $ruta = $destPath . $this->getFileName();
+        }
+        if (false === move_uploaded_file($this->file["tmp_name"], $ruta)) {
+            throw new FileException("No se puede mover el fichero a su destino");
+        }
+    }
+}
